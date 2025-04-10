b16cc7d8e45cc43e22e7b4db7bd9ce3d1c8fb616
Marco-Rocco <marcorocco28@icloud.com>
2025-04-10 15:23:26 +0200

    feat: show list in dom + create select item

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import originalMovies from './data/movies';
 
 function App() {
+  // const [movies, setMovies] = useState()
+
+  useEffect(() => {
+    console.log('nonnt');
+  }, []);
 
   return (
     <>
-
+      <div className="container">
+        <select>
+          <option>select genre</option>
+          {originalMovies.map((movie, index) =>
+            <option key={index}>{movie.genre}</option>
+          )}
+        </select>
+        <ul>
+          {originalMovies.map((movie, index) =>
+            <li key={index}>
+              {movie.title}
+            </li>)}
+        </ul>
+      </div>
     </>
   )
 }
